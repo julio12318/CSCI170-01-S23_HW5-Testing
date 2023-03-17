ba01ff40b3e9e83c8108be6ff33c9b860983bfa5
julio12318 <104460691+julio12318@users.noreply.github.com>
2023-03-17 10:56:58 -0400

    Update hw5test.ml

--- hw5test.ml
+++ hw5test.ml
@@ -1,2 +1,9 @@
 let q1 = TwoListDeque.empty;;
 assert(0=(TwoListDeque.size q1));;
+
+let q2 = TwoListDeque.is_empty;;
+assert(q2);;
+
+let q3 = TwoListDeque.empty;;
+TwoListDeque.add_first 1 q3;;
+assert([1] = TwoListDeque.to_list q3);;
